fc251b4e99209017867c5d1748f9c47c61ce28d3
selmanyazan <selmanyazan@gmail.com>
2022-09-11 20:52:28 +0300

    vat

--- calculating vat/src/Main.java
+++ calculating vat/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double vat;
+        int price;
+        double vatprice;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ürün fiyatını giriniz : ");
+        price = input.nextInt();
+        vat = price<1000 ? 0.18:0.08;
+        vatprice = price + (price*vat);
+        System.out.println("KDV oranınız : " + vat);
+        System.out.println("Fiyat : " + price);
+        System.out.println("KDV dahil fiyatı " + vatprice);
+
+
+
     }
 }
