b858c0858342ca4173a22931d8495d588f74b0ef
selmanyazan <selmanyazan@gmail.com>
2022-09-12 21:48:07 +0300

    userInput

--- userinput/src/Main.java
+++ userinput/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String username, password, newPassword;
+        int choice;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Kullanıcı adı giriniz : ");
+        username = scan.nextLine();
+
+        System.out.println("Şifre agiriniz : ");
+        password = scan.nextLine();
+
+        if (!password.equals("deneme")) {
+            System.out.println("Şifrenizi sıfırlamak ister misiniz");
+            System.out.println("1 - Evet\n2 - Hayir");
+            choice = scan.nextInt();
+            if (choice == 1) {
+                Scanner inp = new Scanner(System.in);
+                System.out.println("yeni şifreyi giriniz");
+                newPassword = inp.nextLine();
+                System.out.println("d"+newPassword);
+                if (newPassword.equals("deneme")) {
+                    System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+                } else {
+                    System.out.println("Şifre oluşturuldu");
+                }
+            }
+        }
     }
 }
