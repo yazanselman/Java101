426260fde965229911be96de9ea98ee4ce405295
selmanyazan <selmanyazan@gmail.com>
2022-09-12 18:47:05 +0300

    taximeter

--- taximeter/src/Main.java
+++ taximeter/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double km;
+        double ucret;
+        double total;
+        double start = 10;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Km giriniz : ");
+        km = input.nextInt();
+
+        ucret = start + (km*2.2);
+        total = ucret>20?ucret:20;
+
+        System.out.println(total);
     }
 }
