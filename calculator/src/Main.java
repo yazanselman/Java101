2ce3279806cfc56947a9fa0a2dcb9c6f409042e8
selmanyazan <selmanyazan@gmail.com>
2022-09-12 21:34:46 +0300

    calculator

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a;
+        int b;
+        int process;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("İlk sayıyı giriniz : ");
+        a = scan.nextInt();
+        System.out.println("İkinci sayıyı giriniz : ");
+        b = scan.nextInt();
+
+        System.out.println("İşlem Seçiniz : ");
+        System.out.println("1 - Toplama\n2 - Çıkarma\n3 - Çarpma\n4 - Bölme");
+        process = scan.nextInt();
+
+        switch (process) {
+            case 1:
+                System.out.println(a + b);
+            case 2:
+                System.out.println(a - b);
+            case 3:
+                System.out.println(a * b);
+            case 4:
+                System.out.println(a / b);
+            default:
+                break;
+        }
+
     }
 }
