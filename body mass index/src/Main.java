8dde4fa6356b03de6a10292fe03facb756edacb2
selmanyazan <selmanyazan@gmail.com>
2022-09-12 19:07:22 +0300

    body mass

--- body mass index/src/Main.java
+++ body mass index/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double boy;
+        double kilo;
+        double index;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz :");
+        boy = scan.nextDouble();
+
+        System.out.println("Lütfen kilonuzu giriniz :");
+        kilo = scan.nextDouble();
+
+        index = kilo / (boy*boy);
+        System.out.println("vücut indeksiniz :  " + index);
+
     }
 }
