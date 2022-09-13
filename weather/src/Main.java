64575419984a178d7b81c9815f39ae9bf8f88474
selmanyazan <selmanyazan@gmail.com>
2022-09-13 07:02:07 +0300

    weather

--- weather/src/Main.java
+++ weather/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        int weather;
+
+        Scanner scan = new Scanner(System.in);
+        weather = scan.nextInt();
+
+        if(weather<5){
+            System.out.println("Kayak");
+        }else if(weather<15){
+            System.out.println("Sinema");
+        }else if(weather<25){
+            System.out.println("piknik");
+        }else{
+            System.out.println("Yüzme");
+        }
+
     }
 }
