1f22be5120562129f78de2cf0fe87d66675cdecc
selmanyazan <selmanyazan@gmail.com>
2022-09-13 22:11:57 +0300

    armstrong

--- armstrong number/src/Main.java
+++ armstrong number/src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int r;
+        int basamak = 0;
+        int t,m;
+        int k;
+        int result = 0;
+        int sonuc = 1;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("sayı giriniz :");
+        r = scan.nextInt();
+        k = r;
+        m = r;
+
+
+        while (k != 0) {
+            k /= 10;
+            basamak++;
+
+        }
+
+        while (m != 0) {
+            t = m % 10;
+            m /= 10;
+            //  System.out.println(t);
+            sonuc = 1;
+            for (int i = 1; i <= basamak; i++) {
+                sonuc *= t;
+            }
+            result += sonuc;
+
+        }
+
+        if (result == r) {
+            System.out.println(r + " sayısı bir Armstrong sayıdır.");
+        } else {
+            System.out.println(r + " sayısı bir Armstrong sayısı değildir.");
+        }
+
     }
 }
