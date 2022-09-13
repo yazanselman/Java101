a79fc93a6240e68472cbe6a9bdfdf86fd430c902
selmanyazan <selmanyazan@gmail.com>
2022-09-13 07:10:20 +0300

    sorting number

--- sorting numbers/src/Main.java
+++ sorting numbers/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        int a,b,c;
+
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+
+        if(a>b && a>c){
+            if(b>c){
+                System.out.println("a>b>c");
+            }else{
+                System.out.println("a>c>b");
+            }
+        } 
+        else if(b>a && b>c){
+            if(a>c){
+                System.out.println("b>a>c");
+            }else{
+                System.out.println("b>c>a");
+            }
+        }
+        else if(c>a && c>b){
+            if(a>b){
+                System.out.println("c>a>b");
+            }else{
+                System.out.println("c>b>a");
+            }
+        }
+
     }
 }
