a20068bc6392aee1aaf1f363a44150423e556c83
selmanyazan <selmanyazan@gmail.com>
2022-09-13 22:43:49 +0300

    harmonic

--- harmonic numbers/src/Main.java
+++ harmonic numbers/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n;
+        double result = 0.0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("sayı giriniz :");
+        n = scan.nextInt();
+
+        for (double i = 1; i <= n; i++) {
+            result += (1 / i);
+        }
+
+        System.out.println(result);
+
     }
 }
