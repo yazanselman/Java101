4761348a78dc75e2413b0fe326ecae2a4f30a8bc
selmanyazan <selmanyazan@gmail.com>
2022-09-13 19:59:19 +0300

    4 and 5 expo.

--- find 2*/src/Main.java
+++ find 2*/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a;
+
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        for (int i = 1; i < a; i*=4) {
+            System.out.println(i);
+            }
+        for (int i = 1; i < a; i*=5) {
+            System.out.println(i);
+        }
+        }
     }
 }
