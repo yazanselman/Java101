5c6d57979969949e522fd636c0bb08f1807ccf5c
selmanyazan <selmanyazan@gmail.com>
2022-09-13 20:46:01 +0300

    expo

--- exponantial number/src/Main.java
+++ exponantial number/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int r;
+        int n;
+        int sonuc = 1;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("sayı giriniz :");
+        n = scan.nextInt();
+        System.out.println("üs giriniz :");
+        r = scan.nextInt();
+
+        for (int i = 1; i < r; i++) {
+            sonuc *= n;
+        }
+
+        System.out.println(sonuc);
     }
 }
