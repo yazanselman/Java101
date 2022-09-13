4833b35613ddec31e2eae77fc5a0c5efde150274
selmanyazan <selmanyazan@gmail.com>
2022-09-13 07:22:39 +0300

    leap year

--- leap year/src/Main.java
+++ leap year/src/Main.java
@@ -1,5 +1,35 @@
+
+
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean leapYear = false;
+        int year;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen yıl giriniz: ");
+        year = input.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0){
+                    leapYear = true;
+                }else{
+                    leapYear = false;
+                }
+            }else {
+                leapYear = true;
+            }
+        } else{
+            leapYear = false;
+        }
+
+        if (leapYear){
+            System.out.println(year + " yılı bir artık yıldır !");
+        }else{
+            System.out.println(year + " yılı bir artık yıl değildir !");
+        }
     }
 }
