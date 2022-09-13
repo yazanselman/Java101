22300433798055c997de3c17b6ea731777822ad1
selmanyazan <selmanyazan@gmail.com>
2022-09-13 19:52:55 +0300

    sum odd number

--- find odd number/src/Main.java
+++ find odd number/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a;
+        int toplam = 0;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            a = scan.nextInt();
+            if (a % 4 == 0) {
+                toplam += a;
+            }
+        } while (a % 2 == 0);
+        System.out.println(toplam);
     }
 }
