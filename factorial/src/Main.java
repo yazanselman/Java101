52b311fdc92bc2d174d25491e127056855204efc
selmanyazan <selmanyazan@gmail.com>
2022-09-13 20:42:42 +0300

    factoriel

--- factorial/src/Main.java
+++ factorial/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int r;
+        int n;
+        int nFactoriel = 1;
+        int rFactoriel = 1;
+        int nrFactoriel = 1;
+        double sonuc;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("n :");
+        n = scan.nextInt();
+        System.out.println("r :");
+        r = scan.nextInt();
+
+
+
+        for (int i = 1; i <= n; i++) {
+            nFactoriel *= i;
+            }
+        for (int i = 1; i <= r; i++) {
+            rFactoriel *= i;
+        }
+        for (int i = 1; i <= n-r; i++) {
+            nrFactoriel *= i;
+        }
+        sonuc = nFactoriel / (rFactoriel - nrFactoriel);
+        System.out.println(sonuc);
+
+        }
     }
 }
