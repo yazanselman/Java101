6d7e627edc7b1e0741871cde66edb3f1653d0c96
selmanyazan <selmanyazan@gmail.com>
2022-09-13 19:32:45 +0300

    even number

--- find even number/src/Main.java
+++ find even number/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a;
+        int count = 0;
+        int toplam = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz");
+
+        a = scan.nextInt();
+
+        for(int b = 1 ; b<a ; b++){
+            if(b%12 == 0){
+                count++;
+                toplam += b;
+            }
+          
+        }
+        System.out.println(toplam/count);
     }
 }
