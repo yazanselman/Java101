3f9f2f697ed3cb51b2907c07fda551ae33e85642
selmanyazan <selmanyazan@gmail.com>
2022-09-14 21:29:46 +0300

    triangle

--- inverted triangle/src/Main.java
+++ inverted triangle/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int k,n,l,m;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Basamak sayısını giriniz : ");
+        n = scan.nextInt();
+
+
+        for (k = 1; k <= n; k++) {
+            for (l = 1; l <= k; l++)//döngümüz ile baklava diliminin alt kısımının sol tarafındaki boşlukları belirtiyoruz.
+            {
+                System.out.print(" ");
+            }
+            for (m = n; m > k; m--)//bu döngümüz ile baklava diliminin alt kısımındaki yıldızları ve arasındaki boşlukları belirtiyoruz.
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
     }
 }
