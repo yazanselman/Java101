d17342447bd3194064e952cd555443fe33e059c7
selmanyazan <selmanyazan@gmail.com>
2022-09-14 22:14:11 +0300

    fibonacci

--- fibonacci numbers/src/Main.java
+++ fibonacci numbers/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        int a,c,b,vEnd;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Girmek istediğiniz ilk sayıyı giriniz: ");
+        a = scan.nextInt();
+        b=a+1;
+
+
+
+        System.out.print("Serinin bitmesini istediğiniz sayıyı giriniz: ");
+        vEnd = scan.nextInt();
+
+        for (int i=a; i<vEnd;a++){
+            System.out.print(i+ " ");
+            c=i+b;
+            i=b;
+            b=c;
+        }
     }
 }
