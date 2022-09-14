981d195301e9b1e93c15b46f8e139363603bcfc7
selmanyazan <selmanyazan@gmail.com>
2022-09-14 22:08:24 +0300

    prime

--- prime numbers/src/Main.java
+++ prime numbers/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        int number;
+        int sayac;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz : ");
+        number = scan.nextInt();
+
+
+        while(number > 2) {
+
+            sayac = 0;
+            for(int i = 2; i < number; i++)
+            {
+                if(number % i == 0) {
+                    sayac++;
+                }
+            }
+            if(sayac == 0) {
+                System.out.println(number + " asal bir sayidir.");
+            }
+            number--;
+        }
     }
 }
