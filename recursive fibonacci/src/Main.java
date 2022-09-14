af85a11ea6cc94aea90325a41c9a988651996c97
selmanyazan <selmanyazan@gmail.com>
2022-09-14 23:10:12 +0300

    recursive fibonacci

--- recursive fibonacci/src/Main.java
+++ recursive fibonacci/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
+
+    static int fib(int n){
+
+        if(n==1 || n==2){
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(fib(5));
     }
 }
