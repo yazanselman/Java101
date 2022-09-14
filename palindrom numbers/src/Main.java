5625ff3ef4c41218d3ace6425919fd5382eaeaf8
selmanyazan <selmanyazan@gmail.com>
2022-09-14 22:53:32 +0300

    palindrom

--- palindrom numbers/src/Main.java
+++ palindrom numbers/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+            static boolean isPalindrom(int number) {
+                int temp = number, reverseNumber = 0, lastNumber;
+
+                while (temp != 0) {
+                    lastNumber = temp % 10;
+                    reverseNumber = (reverseNumber * 10) + lastNumber;
+                    temp /= 10;
+                }
+                if (number == reverseNumber) {
+                    System.out.println("Palindrom sayıdır.");
+                    return true;
+                } else {
+                    System.out.println("Palindrom sayı değildir.");
+                    return false;
+                }
+            }
+            public static void main(String[] args) {
+                Scanner inp = new Scanner(System.in);
+                System.out.print("Bir sayı girin:");
+                int n = inp.nextInt();
+                System.out.println(isPalindrom(n));
+
+
     }
+
 }
+
+
+
