eb294d5c19e27f47724f204a592cdd9b82b8cf35
selmanyazan <selmanyazan@gmail.com>
2022-09-14 21:24:17 +0300

    max min

--- find min max number/src/Main.java
+++ find min max number/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int counter, number, min = 1, max = 1;
+
+        System.out.print("Kac tane sayi gireceksiniz: ");
+        counter = input.nextInt();
+
+        for (int i = 1; i <= counter; i++){
+            System.out.print(i + ".sayiyi giriniz: ");
+            number = input.nextInt();
+
+            if (i == 1){
+                max = number;
+                min = number;
+            }
+
+            if (number > max){
+                max = number;
+            }
+
+            if (number < min){
+                min = number;
+            }
+        }
+
+        System.out.println("En buyuk sayi: " + max);
+        System.out.println("En kucuk sayi: " + min);
     }
 }
