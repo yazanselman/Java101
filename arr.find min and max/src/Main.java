5f2b7f5b67afa30861b860abe43f7e9cf5e1c805
selmanyazan <selmanyazan@gmail.com>
2022-09-15 22:26:40 +0300

    arr find min max

--- arr.find min and max/src/Main.java
+++ arr.find min and max/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        int[] list = {56, 34, 1, 8, 101, -2, -33};
+        Arrays.sort(list);
+
+        int r;
+        Scanner scan = new Scanner(System.in);
+        r = scan.nextInt();
+
+        for (int i : list) {
+            if (i > r) {
+                int index = Arrays.binarySearch(list, i);
+                System.out.println(index);
+                System.out.println("Girilen sayıdan küçük en yakın sayı : " +list[index-1] );
+                System.out.println("Girilen sayıdan büyük en yakın sayı : " + i);
+                break;
+            }
+        }
     }
 }
