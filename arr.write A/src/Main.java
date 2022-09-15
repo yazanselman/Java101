e567bac16fb9a46671cfc032ec7ab95cfc0afd4a
selmanyazan <selmanyazan@gmail.com>
2022-09-15 22:30:30 +0300

    write B

--- arr.write A/src/Main.java
+++ arr.write A/src/Main.java
@@ -1,5 +1,29 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[][] letter = new String[7][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 3 || i == 6) {
+                    letter[i][j] = " * ";
+                    letter[0][3] = "   ";
+                    letter[3][3] = "   ";
+                    letter[6][3] = "   ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+
+                }
+            }
+        }
+
+        for (String[] row : letter) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
     }
 }
