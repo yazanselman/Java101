3bac75838a4bff77baa804a9b776c803cead394c
selmanyazan <selmanyazan@gmail.com>
2022-09-15 22:51:22 +0300

    arr sorting

--- arr.sorting/src/Main.java
+++ arr.sorting/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int boyut;
+        int i = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("boyut : ");
+        boyut = scan.nextInt();
+
+
+        if (boyut > 0) {
+            int arr[] = new int[boyut];
+            for (int j = 0; j <= boyut; j++) {
+                if (i < j) {
+                    System.out.println(j + ". elemanı giriniz");
+                    arr[i] = scan.nextInt();
+                    i++;
+                }
+            }
+            Arrays.sort(arr);
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
