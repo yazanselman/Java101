4e1a70dd7e8a7e837737bb54e04ad5dbe5113a8f
selmanyazan <selmanyazan@gmail.com>
2022-09-24 21:12:18 +0300

    adventure game

---
+++ AdventureGame/Bear.java
@@ -0,0 +1,8 @@
+
+public class Bear extends Obstacle {
+
+	public Bear() {
+		super("Ayý", 7, 20, 12, 2);
+	}
+
+}
--- train/src/Main.java
+++ train/src/Main.java
@@ -1,35 +1,12 @@
-import java.util.*;
-import java.io.*;
+import java.util.Random;
 
-class Main {
+public class Main {
 
-    public static String LetterCapitalize(String str) {
+    public static void main(String[] args) {
 
-        String[] kelime = str.split(" ");
-        String k="";
+        Random r=new Random();
+        int a=r.nextInt(2);
+        System.out.println(a);
 
-        for(String t : kelime){
-            k += t.substring(0,1).toUpperCase() + t.substring(1).toLowerCase() +" ";
-
-        }
-        // code goes here
-        return k;
     }
-
-    public static void main (String[] args) {
-        // keep this function call here
-        Scanner s = new Scanner(System.in);
-      //  System.out.print(LetterCapitalize(s.nextLine()));
-
-        int a = 5;
-        int b = 9;
-        int c = 0;
-        if (a > b) {
-            c = a;
-            a = b;
-            b = c;
-        }
-        System.out.println(b + "," + a);
-    }
-
 }
